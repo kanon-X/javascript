e43d1ae8302ae3e0b52f028f00eadcfa10552da9
kanon-X <syedur.b@uttarauniversity.edu.bd>
2025-08-21 23:03:08 +0600

    initial commit

---
+++ else.js
@@ -0,0 +1,10 @@
+var a = 100
+var b = 20
+
+if(a>b){
+    console.log(a +' is greater then : ' +b)
+    
+}
+else{
+    console.log(b + ' is greater then : ' + a)
+}
---
+++ if.js
@@ -0,0 +1,15 @@
+var a = 100
+var b = 20
+
+if(a>b){
+    console.log(a +' is greater then : ' +b)
+    
+}
+
+var n = 5
+if(n%2 ==0){
+    console.log(n + ' is even')
+}
+if(n %2 ==1){
+    console.log(n + ' is odd')
+}
