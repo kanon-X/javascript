a1d1fce2c6edb5c67b1d7a84964cd8ba20eb4ffb
kanon-X <syedur.b@uttarauniversity.edu.bd>
2025-08-24 23:09:36 +0600

    initial commit

---
+++ ArrayTraverse.js
@@ -0,0 +1,13 @@
+var arr= [1,2,3,4,5]
+
+for (var i=0; i< arr.length; i++){
+    arr[i] = arr[i] + 2
+   
+}
+ console.log(arr)
+
+ for (var i =0; i<arr.length; i++){
+    if(arr[i] %2 ==0){
+       console.log(arr[i])
+    }
+ }
