e82e6f16229188038e376558063b4ccc531017ba
kanon-X <syedur.b@uttarauniversity.edu.bd>
2025-08-26 20:22:23 +0600

    initial commit

---
+++ arguments.js
@@ -0,0 +1,25 @@
+function add( a ,b)
+{
+var result = a+b
+console.log(result)
+}
+add(48,89)
+add(100,200)
+
+
+var arr1 = [1,2,34,4]
+var arr2 = [3,4,5,6]
+var arr3 = [98,45,98,77]
+var sum1 = 0
+function sumofArray(arr){
+    var sum =0
+     for(var i = 0; i< arr.length;i++){
+        sum+= arr[i]
+     }
+     console.log(sum)
+}
+sumofArray(arr1)
+sumofArray(arr2)
+sumofArray(arr3)
+
+
